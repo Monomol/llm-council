4e59c41dff41903bff69b7cd6edb6c8732dfb304
Michal Louda <xlouda@fi.muni.cz>
2026-03-18 17:29:06 +0100

    fix: wrong log path

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ from .config import ALLOWED_TOKENS
 from .objects import ProcessRequest
 
 logging.basicConfig(
-    filename="logs/log.txt",
+    filename="data/logs/log.txt",
     level=logging.INFO,
     format="%(asctime)s [%(name)s] [%(levelname)s] %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
