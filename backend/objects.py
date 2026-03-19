147b7d6988f0924d30af333d202cfa0b99300904
Michal Louda <xlouda@fi.muni.cz>
2026-03-19 16:04:16 +0100

    fix: default None values missing in request definition

--- backend/objects.py
+++ backend/objects.py
@@ -4,6 +4,6 @@ from typing import Set, Optional
 # This exists to prevent the circular dependency between main nad db
 class ProcessRequest(BaseModel):
     pipe_id: str
-    submit_ids: Optional[Set[int]]
-    student_emails: Optional[Set[str]]
-    head_n_results: Optional[int]
+    submit_ids: Optional[Set[int]] = None
+    student_emails: Optional[Set[str]] = None
+    head_n_results: Optional[int] = None
